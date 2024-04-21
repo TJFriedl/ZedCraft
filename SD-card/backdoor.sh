a81b7c99ad70a23edbd6f2a8ec35010531908427
Justin Templeton <jstemps02@gmail.com>
2024-04-21 17:21:14 -0500

    added some comments to the backdoor

--- SD-card/backdoor.sh
+++ SD-card/backdoor.sh
@@ -10,19 +10,22 @@ exec 1>$tmpPath
 
 tail -f logs/latest.log | while read p; do
 	if grep ">>" <<<$p >> /dev/null; then 
+		#remove everything before the ">>"
 		p=`sed 's/.*>>//' <<<$p`
+		#save the first character in the user input
 		first=${p:0:1}
+		#turret mode
 		if [ "$first" = "!" ]; then
 			byte=${p:1:3}
 			de="echo -en '\x${byte}' > /dev/launcher0"
 			$de
 			echo $de>&3
-
+		#minecraft input mode
 		elif [ "$first" = "0" ]; then
  			command="${p:1}"
 			echo $command >&3
                         echo $command > $sockPath
-			
+		#main backdoor mode
 		else  	
 			command=$p
         		$command
